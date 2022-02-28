d71241d73c1de1de970e67f8d2d052e67e430890
Remi776 <99408740+Remi776@users.noreply.github.com>
2022-02-28 20:47:35 +0200

    add ex009

---
+++ ex_9/Program.cs
@@ -0,0 +1,39 @@
+﻿void FillPrintArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        array[index] = new Random().Next(10, 100);
+        Console.Write(array[index] + " ");
+        index++;
+    }
+}
+
+void FindMax(int[] arr)
+{
+    int len = arr.Length;
+    int i = 0;
+    while (i < len)
+    {
+        if (arr[i] % 10 > arr[i] / 10)
+        {
+            Console.Write(arr[i]+"->"+" ");
+            Console.WriteLine(arr[i] % 10);
+        }
+        else
+        {
+            Console.Write(arr[i]+"->"+" ");
+            Console.WriteLine(arr[i] / 10);
+        }
+        i++;
+    }
+}
+
+int[] array = new int[10];
+
+FillPrintArray(array);
+Console.WriteLine();
+FindMax(array);
+
+
