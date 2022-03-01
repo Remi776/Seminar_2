8c6b90bf887b8541638f033da9616883b9604057
Remi776 <99408740+Remi776@users.noreply.github.com>
2022-03-01 20:25:11 +0200

    add ex015

---
+++ home_work/ex_15/Program.cs
@@ -0,0 +1,21 @@
+﻿void Weekend(int number)
+{
+    string[] weekdays = { "Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday" };
+    if (number == 5)
+    {
+        Console.WriteLine("Shabbat shalom!");
+    }
+    else if (number == 6)
+    {
+        Console.WriteLine("My head... i need water! What happend yesterday?");
+    }
+    else
+    {
+        Console.WriteLine("Oh no, I have to go for a work;(");
+    }
+}
+
+Console.Write("Enter the day number of the week: ");
+int n = int.Parse(Console.ReadLine());
+
+Weekend(n);
