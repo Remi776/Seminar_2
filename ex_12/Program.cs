ce43788428d35e43df2ba7fb026ae0667857d2a9
Remi776 <99408740+Remi776@users.noreply.github.com>
2022-03-01 20:51:17 +0200

    add ex012

---
+++ ex_12/Program.cs
@@ -0,0 +1,18 @@
+﻿void Compare(int a, int b)
+{
+    if (a % b == 0)
+    {
+        Console.WriteLine($"{a} кратно {b}");
+    }
+    else
+    {
+        Console.WriteLine($"{a} не кратно {b}, остаток {a % b}");
+    }
+}
+
+Console.Write("Enter number 1: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("\nEnter number 2: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+Compare(n1, n2);
