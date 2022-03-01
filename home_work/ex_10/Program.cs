43ddd9561eb5a1a7cdc0c2af24cbdf069d8b0fce
Remi776 <99408740+Remi776@users.noreply.github.com>
2022-03-01 20:31:33 +0200

    add ex010

--- home_work/ex_10/Program.cs
+++ home_work/ex_10/Program.cs
@@ -1,9 +1,16 @@
 ﻿void Versa(int number)
 {
-    string last_digit = Convert.ToString(number % 10);
-    string middle_digit = Convert.ToString(number / 10 % 10);
-    string first_digit = Convert.ToString(number / 100);
-    Console.WriteLine(last_digit + middle_digit + first_digit);
+    if (number < 100 || number > 999)
+    {
+        Console.WriteLine("Wrong number, try again");
+    }
+    else
+    {
+        string last_digit = Convert.ToString(number % 10);
+        string middle_digit = Convert.ToString(number / 10 % 10);
+        string first_digit = Convert.ToString(number / 100);
+        Console.WriteLine(last_digit + middle_digit + first_digit);
+    }
 }
 
 Console.Write("Enter three-digit number: ");
