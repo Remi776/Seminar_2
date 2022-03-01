15d75e2b297c42b11112ff140b590c61af344834
Remi776 <99408740+Remi776@users.noreply.github.com>
2022-03-01 16:43:58 +0200

    add home_work

---
+++ home_work/ex_10/Program.cs
@@ -0,0 +1,12 @@
+﻿void Versa(int number)
+{
+    string last_digit = Convert.ToString(number % 10);
+    string middle_digit = Convert.ToString(number / 10 % 10);
+    string first_digit = Convert.ToString(number / 100);
+    Console.WriteLine(last_digit + middle_digit + first_digit);
+}
+
+Console.Write("Enter three-digit number: ");
+int a = int.Parse(Console.ReadLine());
+Versa(a);
+
