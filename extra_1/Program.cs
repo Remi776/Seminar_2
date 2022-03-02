9dea54d0838f2a9282b54b96303a6540d2edd00e
Remi776 <99408740+Remi776@users.noreply.github.com>
2022-03-02 17:38:05 +0200

    add extra001

---
+++ extra_1/Program.cs
@@ -0,0 +1,20 @@
+﻿async void IsoscelesTriangle(int a, int b, int c)
+{
+    if ((a + b > c || a + c > b || b + c > a) & (a == b || a == c || c == b))
+    {
+        Console.WriteLine("\nIsosceles triangle");
+    }
+    else
+    {
+        Console.WriteLine("\nThe triangle does not exist");
+    }
+}
+
+Console.Write("Side length 'a': ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("\nSide length 'b': ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("\nSide length 'c': ");
+int num3 = int.Parse(Console.ReadLine());
+
+IsoscelesTriangle(num1, num2, num3);
